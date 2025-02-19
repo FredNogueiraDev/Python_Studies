6a7c3a88a3a92dbac9edb039e02dc2711df5b2b2
Frederico Nogueira <fredericonogueira01@outlook.com>
2025-02-19 07:18:05 -0300

    Operadores matemáticos.

---
+++ day_2.py
@@ -0,0 +1,28 @@
+num_1 = 9
+num_2 = 150
+
+resultado = num_2 + num_1
+
+print(resultado)
+
+divisao = num_2 / num_1
+inteiro = num_2 // num_1
+resto = num_2 % num_1
+
+print(divisao)
+print(inteiro)
+print(resto)
+
+alunos_presentes = 23
+alunos_ausentes = '8'
+
+total_alunos = alunos_presentes + int(alunos_ausentes)
+print('Total de alunos:', total_alunos)
+
+
+tempo = 3.85960
+print('Tempo arredondado:', round(tempo))
+
+print('2^3:', pow(2, 3))
+
+print(sum([1,2,3,4,5,6,7]))
