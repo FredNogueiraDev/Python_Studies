a6787e2cf2e79551b708f9191236aa0bf41880fb
Frederico Nogueira <fredericonogueira01@outlook.com>
2025-02-19 06:53:59 -0300

    Variáveis, comentários e tipos.

---
+++ day_1.py
@@ -0,0 +1,22 @@
+#Comentário
+
+'''
+Comentário com mais de
+uma linha
+'''
+
+print('Fred Nog')
+
+nome = "Fred"
+sobrenome = "Nogueira"
+idade = 21
+peso = 78.5
+ativo = True
+resultado = None
+
+print(nome, sobrenome)
+print(type(idade))
+print(type(peso))
+print(type(ativo))
+
+print('Meu nome é', nome, 'e tenho', idade, 'anos')
